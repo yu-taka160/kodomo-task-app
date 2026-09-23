e585b35bf7cfa02d1a2ba10552460f177185a526
yu-taka160 <yumiu0821@icloud.com>
2026-09-23 16:29:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ st.markdown(
 # =========================
 #     アプリ本体
 # =========================
-st.markdown("<div style='height:20px;'></div>", unsafe_allow_html=True)
+st.markdown("<div style='height:10px;'></div>", unsafe_allow_html=True)
 st.markdown("<h1 style='color:#FF8C00;'>こどもタスクチェックアプリ</h1>", unsafe_allow_html=True)
 
 # --- タスクを保存するための session_state ---
