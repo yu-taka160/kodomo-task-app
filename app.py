1f94e90ef3541b03fe11985115f4c70ccc078022
yu-taka160 <yumiu0821@icloud.com>
2026-09-23 15:09:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ st.markdown(
 # =========================
 #     アプリ本体
 # =========================
-st.markdown("<div style='height:60px;'></div>", unsafe_allow_html=True)
+st.markdown("<div style='height:80px;'></div>", unsafe_allow_html=True)
 
 st.title("こどもタスクチェックアプリ")
 
