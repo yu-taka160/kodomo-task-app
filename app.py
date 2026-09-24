acde7940f928298da1c69cf20927de7ef7d62950
yu-taka160 <yumiu0821@icloud.com>
2026-09-24 13:12:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ pc_animals = ["🐰", "🐣", "🐧"]
 # スマホ用キャラセット
 mobile_animals = ["🐹", "🐶", "🐷"]
 
+load_dotenv()
+
+# --- スマホ判定（CSS＋hidden input方式） ---
 st.markdown("""
 <style>
 @media screen and (max-width: 600px) {
@@ -41,9 +44,6 @@ window.history.replaceState({}, "", `${location.pathname}?${params}`);
 
 params = st.query_params
 is_mobile = params.get("mobile_flag") == "true"
-
-load_dotenv()
-
 endpoint = os.getenv("AZURE_LANGUAGE_ENDPOINT")
 key = os.getenv("AZURE_LANGUAGE_KEY")
 region = os.getenv("AZURE_LANGUAGE_REGION")
